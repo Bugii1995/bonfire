2e99f5e6c9506e4542e1ff9df2bebb87805e7838
bugii1995 <bukhbilegt@gmail.com>
2026-01-09 18:59:13 +0800

    Initial frontend setup with Svelte, Tailwind, Flowbite

---
+++ frontend/src/lib/components/Footer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Footer, FooterCopyright, FooterLinkGroup, FooterLink, DarkMode } from "flowbite-svelte";
+</script>
+
+<Footer>
+  <FooterCopyright href="/" by="Chocolingo™" year={2026} />
+  <FooterLinkGroup class="mt-3 flex flex-wrap items-center text-sm text-gray-500 sm:mt-0 dark:text-gray-400">
+    <FooterLink href="/">About</FooterLink>
+    <FooterLink href="/">Privacy Policy</FooterLink>
+    <FooterLink href="/">Licensing</FooterLink>
+    <FooterLink href="/">Contact</FooterLink>
+    <DarkMode />
+  </FooterLinkGroup>
+</Footer>
---
+++ frontend/src/lib/components/Navbar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { page } from "$app/state";
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
+  let activeUrl = $derived(page.url.pathname);
+  let activeClass = "text-white bg-green-700 md:bg-transparent md:text-green-700 md:dark:text-white dark:bg-green-600 md:dark:bg-transparent";
+  let nonActiveClass =
+    "text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
+</script>
+
+<Navbar>
+  <NavBrand href="/">
+    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">🍫 Chocolingo</span>
+  </NavBrand>
+  <NavHamburger />
+  <NavUl {activeUrl} classes={{ active: activeClass, nonActive: nonActiveClass }}>
+    <NavLi href="/">Home</NavLi>
+    <NavLi href="/contact">Study</NavLi>
+    <NavLi href="/dashboard">Dashboard</NavLi>
+    <NavLi href="/leaderboard">Leaderboard</NavLi>
+    <NavLi href="/account">Account</NavLi>
+  </NavUl>
+</Navbar>
